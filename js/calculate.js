15dc8883c15d5cd6ab0ed791235948f37d05489f
kuoalan <alan@alankuo.com>
2022-05-10 00:13:53 -0400

    Update calculate.js
    - Fixed bug with setting null inputs to 0 (or 1 for days worked)
    
    - Fixed bug with calculating daily pay correctly with 3rd molar extractions

--- js/calculate.js
+++ js/calculate.js
@@ -10,9 +10,9 @@ function getFormData() {
 }
 
 function calculatePaycheck(monthlyProduction, invisalign, surgicalThirds, simpleThirds, daysWorked, healthReimbursement, adjustments) {
-	for (var i = 0, j = arguments.length; i < j; i++) {
-		if (!arguments[i] || arguments[i].length ===0) {
-			if (i = 4) {
+	for (var i = 0; i < 7; i++) {
+		if (!arguments[i]) {
+			if (i == 4) {
 				arguments[i] = 1;
 			}
 			else {
@@ -26,8 +26,8 @@ function calculatePaycheck(monthlyProduction, invisalign, surgicalThirds, simple
 	var topTier = Math.max(0, adjustedDailyProduction - 3500);
 	var midTier = Math.max(0, adjustedDailyProduction - topTier - 2500);
 	var lowTier = Math.max(0, adjustedDailyProduction - topTier - midTier);
-	var dailyPay = (topTier * 0.4 + midTier*0.35 + lowTier*0.3 + (simpleThirds * 86 * 0.4) + (surgicalThirds * 140 * 0.4)).toFixed(2);
-	var monthlyPaycheck = ((dailyPay*daysWorked) + parseFloat(healthReimbursement) + parseFloat(adjustments) + (simpleThirds*86*0.4) + (surgicalThirds * 140 * 0.4)).toFixed(2)
+	var dailyPay = (Math.max((topTier * 0.4 + midTier*0.35 + lowTier*0.3 + ((simpleThirds * 86 * 0.4)/daysWorked) + ((surgicalThirds * 140 * 0.4)/daysWorked)), 800.00)).toFixed(2);
+	var monthlyPaycheck = ((dailyPay*daysWorked) + parseFloat(healthReimbursement) + parseFloat(adjustments)).toFixed(2)
 	document.getElementById("dailyProduction").value = dailyPay
 	document.getElementById("monthlyPaycheck").value = monthlyPaycheck
 	var rawProduction = (monthlyProduction/daysWorked).toFixed(2);
